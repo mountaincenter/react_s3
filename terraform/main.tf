39e144e0ad1241910631021efd23b2649a8f2396
mountaincenter <y.hiroyuki560730@gmail.com>
2023-02-01 09:39:09 +0900

    fist commit

---
+++ terraform/main.tf
@@ -0,0 +1,59 @@
+#===================
+# s3
+#===================
+
+resource "aws_s3_bucket" "web" {
+  bucket        = var.bucket_name
+  force_destroy = true
+  policy = templatefile("bucket-policy.json", {
+    "bucket_name" = var.bucket_name
+  })
+  versioning {
+    enabled = true
+  }
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+  }
+}
+
+resource "aws_s3_bucket_acl" "web" {
+  bucket = aws_s3_bucket.web.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_public_access_block" "web" {
+  bucket                  = aws_s3_bucket.web.id
+  block_public_acls       = true
+  block_public_policy     = false
+  ignore_public_acls      = true
+  restrict_public_buckets = false
+}
+
+# resource "aws_s3_bucket_policy" "main" {
+#   bucket = aws_s3_bucket.web.id
+#   policy = data.aws_iam_policy_document.s3_policy.json
+# }
+
+resource "aws_s3_object" "main" {
+  bucket       = aws_s3_bucket.web.id
+  key          = "index.html"
+  source       = "../index.html"
+  content_type = "text/html"
+  etag         = filemd5("../index.html")
+}
+
+
+# module "distribution_files" {
+#   source   = "hashicorp/dir/template"
+#   base_dir = "../frontend/react-app/build"
+# }
+
+# resource "aws_s3_object" "multiple_objects" {
+#   for_each     = module.distribution_files.files
+#   bucket       = aws_s3_bucket.web.id
+#   key          = each.key
+#   source       = each.value.source_path
+#   content_type = each.value.source_path
+#   etag         = filemd5(each.value.source_path)
+# }
