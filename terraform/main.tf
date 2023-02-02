ded12584791e643878c30cb028fd3e321141c1bd
mountaincenter <y.hiroyuki560730@gmail.com>
2023-02-02 16:35:20 +0900

    modify

--- terraform/main.tf
+++ terraform/main.tf
@@ -46,13 +46,13 @@ resource "aws_s3_bucket_policy" "main" {
 
 
 
-resource "aws_s3_bucket_public_access_block" "web" {
-  bucket                  = aws_s3_bucket.web.id
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
-}
+# resource "aws_s3_bucket_public_access_block" "web" {
+#   bucket                  = aws_s3_bucket.web.id
+#   block_public_acls       = false
+#   block_public_policy     = false
+#   ignore_public_acls      = false
+#   restrict_public_buckets = false
+# }
 
 
 # resource "aws_s3_object" "main" {
