5f65833389ae83b156d461813a80bd7c4ea31419
mountaincenter <y.hiroyuki560730@gmail.com>
2023-02-02 10:03:01 +0900

    finished githuactions terraform s3 file

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -1,3 +1,20 @@
 variable "bucket_name" {
   default = "test-react-yama"
 }
+
+variable "aws_region" {
+  default = "ap-northeast-1"
+}
+
+variabel "domain_name" {
+  default = "ymnk.fun"
+}
+
+locals {
+  fqdn = {
+    web_name = "web.${var.domain_name}"
+  }
+  bucket = {
+    name = local.fqdn.web_name
+  }
+}
