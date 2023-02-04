12e29715be7b728d684aea4e2ce6273d96049203
mountaincenter <y.hiroyuki560730@gmail.com>
2023-02-04 18:24:50 +0900

    create rails_env

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,6 +1,12 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get  '/health_check', to: 'health_checks#index'
+  get  '/blogs',        to: 'blogs#index'
+  get  '/blogs/:id',    to: 'blogs#show'
+  post '/blogs',        to: 'blogs#create'
 end
